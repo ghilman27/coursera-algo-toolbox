e542acccf5dfb45d555ff57c1b32cccd621567f8
Ghilman AL Fatih <ghilman27@gmail.com>
2020-08-16 15:34:14 +0700

    week 2: problem 7 submit

--- week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
+++ week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
@@ -1,22 +1,54 @@
 # Uses python3
-import sys
 
-def fibonacci_partial_sum_naive(from_, to):
-    sum = 0
+START_PATTERN = [0, 1]
+BASIS_MODULO = 10
 
-    current = 0
-    next  = 1
+def get_pisano_sequence_and_period(m):
+    if m < 2:
+        print("WRONG INPUT")
 
-    for i in range(to + 1):
-        if i >= from_:
-            sum += current
+    period = 0
+    sequence = [0, 1]
+    while True:
+        sequence.append( sum(sequence[-2:]) % m )
+        period += 1
+        if sequence[-2:] == START_PATTERN:
+            return sequence[:-2], period
 
-        current, next = next, current + next
 
-    return sum % 10
+def get_cumsum_pisano_sequence_and_period(m):
+    if m < 2:
+        print("WRONG INPUT")
+
+    PISANO_SEQUENCE, _ = get_pisano_sequence_and_period(m)
+    sequence = [0, 1]
+    period = 0
+    cumsum = 1
+    idx = 1
+
+    while True:
+        idx = (idx + 1) % len(PISANO_SEQUENCE)
+        period += 1
+        cumsum = (cumsum + PISANO_SEQUENCE[idx]) % m
+        sequence.append( cumsum )
+        if sequence[-2:] == START_PATTERN:
+            return sequence[:-2], period
+
+
+def fibonacci_partial_sum_last_digit(from_, to):
+    if from_ > to or from_ < 0 or to < 0:
+        print("INVALID INPUT")
+    
+    sequence, period = get_cumsum_pisano_sequence_and_period(BASIS_MODULO)
+
+    cumsum_from = 0
+    if from_ > 0:
+        cumsum_from = sequence[(from_ - 1) % period]
+    cumsum_to = sequence[to % period]
+
+    return (cumsum_to - cumsum_from) % BASIS_MODULO
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read();
-    from_, to = map(int, input.split())
-    print(fibonacci_partial_sum_naive(from_, to))
+    from_, to = map(int, input().split())
+    print(fibonacci_partial_sum_last_digit(from_, to))
