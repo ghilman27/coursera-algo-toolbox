509815866730d0902d021ed06707f30e5613fb96
Ghilman AL Fatih <ghilman27@gmail.com>
2020-08-19 18:38:25 +0700

    week 3: problem 6 submit

--- week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -1,10 +1,18 @@
 # Uses python3
 import sys
 
-def optimal_summands(n):
+def optimal_summands(total_candy):
     summands = []
-    #write your code here
-    return summands
+    num_candy = 1
+    candy_left = total_candy
+    while True:
+        if candy_left - num_candy > num_candy:
+            candy_left -= num_candy
+            summands.append(num_candy)
+            num_candy += 1
+        else:
+            summands.append(candy_left)
+            return summands
 
 if __name__ == '__main__':
     input = sys.stdin.read()
