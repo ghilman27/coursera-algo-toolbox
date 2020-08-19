eb98a74a3c4afebfd7def5e2e8a57067eb6d67aa
Ghilman AL Fatih <ghilman27@gmail.com>
2020-08-19 17:38:18 +0700

    week 3: problem 5 submit

--- week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
+++ week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
@@ -4,13 +4,30 @@ from collections import namedtuple
 
 Segment = namedtuple('Segment', 'start end')
 
+def sort_by_left_end(segments):
+    return sorted(segments)
+
+
 def optimal_points(segments):
+    segments = sort_by_left_end(segments)
     points = []
-    #write your code here
-    for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+    if len(segments) == 1:
+        return segments[0].end
+
+    point_start = segments[0].start
+    point_end = segments[0].end
+    for s in segments[1:]:
+        if s.start > point_start and s.start > point_end:
+            points.append(point_end)
+            point_start = s.start
+            point_end = s.end
+        if s.start > point_start and s.start <= point_end:
+            point_start = s.start
+        if s.end < point_end:
+            point_end = s.end
+    points.append(point_end)
     return points
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -19,3 +36,4 @@ if __name__ == '__main__':
     points = optimal_points(segments)
     print(len(points))
     print(*points)
+
