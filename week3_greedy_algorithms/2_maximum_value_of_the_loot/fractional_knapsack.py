4476302c4d2b534a2c53b63cbb7942d2b46e51a2
Ghilman AL Fatih <ghilman27@gmail.com>
2020-08-19 12:57:17 +0700

    week 3: problem 2 submit

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,10 +1,22 @@
 # Uses python3
 import sys
 
+def sort_densities(weights, values):
+    densities = [value * 1.0 / weight for weight, value in zip(weights, values)]
+    return sorted([(density, index) for index, density in enumerate(densities)], reverse=True)
+
+
 def get_optimal_value(capacity, weights, values):
     value = 0.
-    # write your code here
+    sorted_densities = sort_densities(weights, values)
 
+    for item_density, item_index in sorted_densities:
+        if weights[item_index] <= capacity:
+            capacity -= weights[item_index]
+            value += values[item_index]
+        else:
+            value += capacity * item_density
+            return value
     return value
 
 
@@ -14,4 +26,4 @@ if __name__ == "__main__":
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
     opt_value = get_optimal_value(capacity, weights, values)
-    print("{:.10f}".format(opt_value))
+    print(f"{opt_value:.4f}")
