671dd24fd4131231e21fe16e0ab54376a3430f46
Ghilman AL Fatih <ghilman27@gmail.com>
2020-08-19 10:43:11 +0700

    week 3: problem 1 submit

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,10 +1,17 @@
 # Uses python3
-import sys
 
-def get_change(m):
-    #write your code here
-    return m
+def get_change(coin_value):
+    DENOMINATIONS = [10, 5, 1]
+    min_num_coins = 0
+    for denom in DENOMINATIONS:
+        while denom <= coin_value:
+            coin_value -= denom
+            min_num_coins += 1
+    
+    return min_num_coins
+            
+
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(input())
     print(get_change(m))
