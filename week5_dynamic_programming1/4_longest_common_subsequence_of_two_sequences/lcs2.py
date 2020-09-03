6ffee4166fadc4ec8d3940990ac7fe7def34d18b
Ghilman AL Fatih <ghilman27@gmail.com>
2020-09-03 14:22:08 +0700

    week 5: problem 4 submit

--- week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -1,10 +1,34 @@
-#Uses python3
+"""
+    This is an alignment problem, a variation of edit distance problem
+    best solved with Needleman–Wunsch algorithm
+    we then set the weight:
+    - insertion: 0
+    - deletion: 0
+    - mismatch: 0
+    - match: 1
+
+    and unlike edit distance who compute the minimum number of operation
+    we compute the maximum number of equation
+"""
+
 
 import sys
 
-def lcs2(a, b):
-    #write your code here
-    return min(len(a), len(b))
+def lcs2(s, t):
+    # Construct distance matrix
+    rows, cols = (len(s)+1, len(t)+1)
+    distance_matrix = [[0 for col in range(cols)] for row in range(rows)] 
+
+    for row, s_idx in zip(range(1, rows), range(len(s))):
+        for col, t_idx in zip(range(1, cols), range(len(t))):
+            match_weight = (s[s_idx] == t[t_idx])
+            match = distance_matrix[row-1][col-1] + match_weight
+            insertion = distance_matrix[row][col-1]
+            deletion = distance_matrix[row-1][col]
+            distance_matrix[row][col] = max(match, insertion, deletion)
+    
+    return distance_matrix[len(s)][len(t)]
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
